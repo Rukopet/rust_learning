1fb1028d33ba8a29a3b9c7f02a31f8521e0e5349
Dmitry Shabalkin <dshabalkin@altdevlab.com>
2025-02-08 16:17:11 +0100

    Add user input for guessing game and include rand dependency

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,8 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Something went wrong with the input");
 }
