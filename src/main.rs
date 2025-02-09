e1575e856c9fd42ff05064b19791776da184caac
Dmitry Shabalkin <dshabalkin@altdevlab.com>
2025-02-09 15:26:44 +0100

    add game choose

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod secret_number_game;
+use std::env;
 
 fn main() {
-    secret_number_game::play();
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => secret_number_game::play(),
+        2 => match args[1].as_str() {
+            "guess_number" => secret_number_game::play(),
+            _ => println!("Game doesn't exists {}", args[1]),
+        },
+        _ => println!("Define single game only"),
+    }
 }
